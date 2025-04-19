a82e5ee7ec6904fc71071c38dd975e6cac467e45
Hammad <mhammadafzal3333@gmail.com>
2025-04-19 20:38:32 +0500

    Setup React, Tailwind, shadcn/ui

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+	return (
+		<div className="p-4">
+			<h1 className="text-3xl font-bold underline">URL Shortener</h1>
+			<Button variant="outline" className="mt-4">
+				Test ShadCN Button
+			</Button>
+		</div>
+	);
+}
+
+export default App;
