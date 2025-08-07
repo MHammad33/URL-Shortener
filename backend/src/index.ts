c8f2d83d380ee9004ba575a544eb879d91f9939f
Muhammad Hammad Afzal <hammad.afzal@werplay.com>
2025-08-07 12:44:43 +0500

    Add backend

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import connectDB from "./db";
+import app from "./app";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+connectDB()
+	.then(() => {
+		app.listen(PORT, () => {
+			console.log(`[server]: Server is running at http://localhost:${PORT}`);
+		});
+	})
+	.catch((error) => console.log("DB connection failed", error));
